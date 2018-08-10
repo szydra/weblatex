e8a04e0cdf9333c235d6390c462f83b76b47888f
szydra <szymon.draga@gmail.com>
2018-08-10 20:36:47 +0200

    Add the main Spring Boot class.

---
+++ src/main/java/latex/WebLatexApp.java
@@ -0,0 +1,13 @@
+package latex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebLatexApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebLatexApp.class, args);
+	}
+
+}
